140518a393794ef5f34e9c3dc41ca2146be0cb15
Emmanuel Okyere <chief@hutspace.net>
2011-07-31 13:47:17 +0000

    Fixed Handler implementation

--- vineyard/vineyard.py
+++ vineyard/vineyard.py
@@ -1,10 +1,16 @@
 DATAPORT = '/dev/ttyUSB0'
 CONTROLPORT = '/dev/ttyUSB3'
 
+from simplesms import Handler
 
-class Vineyard():
-    def __init__(selfself, gateway):
+class Vineyard(Handler):
+    def __init__(self, gateway):
        Handler.__init__(self, gateway)
+       
+    def handle_sms(self, message):
+        print 'We have received message from %s that says [%s]' % \
+        (message.sender, message.text)
+        
 
 from simplesms import Modem
 from simplesms import Gateway
@@ -18,12 +24,10 @@ def connect_modems(options=None):
     modem = Modem(port=DATAPORT, control_port=CONTROLPORT)
     modem.boot()
     return {'MTN':modem}
-
-
-        
     
 def main():
     modems, gateway = bootstrap()
+    vineyard = Vineyard(gateway)
     gateway.start()
     
 if __name__ == "__main__":
