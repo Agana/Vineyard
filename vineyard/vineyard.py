bff723bb97e60a0288a348c83838660321a12e76
Agana <nsiire@gmail.com>
2011-08-12 17:53:20 +0000

    prayer, song reqeust, contact, verse, explanation

--- vineyard/vineyard.py
+++ vineyard/vineyard.py
@@ -1,3 +1,7 @@
+# Vineyard - Tending the flock with mobile technology
+# Agana Agana-Nsiire, for The Truth for Free Media Ministry
+# Bible verses obtained from bible.cc
+
 DATAPORT = '/dev/ttyUSB0'
 CONTROLPORT = '/dev/ttyUSB3'
 
@@ -13,50 +17,109 @@ class Vineyard(Handler):
         print 'We have received message from %s that says [%s]' % \
         (message.sender, message.text)
         rec_text = message.text
-        phrase_pastor = re.findall(r'^pastor', rec_text, re.IGNORECASE)
-        phrase_prayer = re.findall(r'^prayer', rec_text, re.IGNORECASE)
+        
+#       These are what to look for in a received message
+        
+#       Request for a pastor's contact
+        phrase_pastor = re.match(r'^pastor\s\w+\s\w+.', rec_text, re.IGNORECASE)
+#       Prayer request
+        phrase_prayer = re.match(r'^prayer.', rec_text, re.IGNORECASE)
+#       Bible verse query
         phrase_bible = re.match(r'^\w+\s\d+:\d+', rec_text, re.IGNORECASE)
+#       Bible verse explanation request
+        phrase_bible_explain = re.match(r'^explain\s\w+\s\d+:\d+', rec_text, re.IGNORECASE)
+#       Song request
+        phrase_hymn = re.match(r'^hymn|sdah\s\d+.', rec_text, re.IGNORECASE)
+        
+#       This handles requests for pastors' contacts
+        
         if phrase_pastor:
-            text = "A pastor will be in touch shortly"
+#       TODO: create a webpage with pastors' names and scrape that with regex
+
+            name_tel = {'Emmanuel Gaizer' : '0244444444', 'Nii Lante Thompson' : '0245444545'}
+            name_email = {'Emmanuel Gaizer' : 'egaizer@yahoo.com', 'Nii Lante Thompson' : 'niilantethompson@yahoo.com'}
+            name_tel.items()
+            pastor_call = rec_text.split()[0]
+            pastor_name = " ".join(rec_text.split()[1:])
+            print pastor_name
+            if pastor_name in name_tel:
+                pastor_send = pastor_name + " " + name_tel[pastor_name] + " " + name_email[pastor_name]
+                text = pastor_send
+                print text
+                self.send(message.sender, text)
+            else:
+                text = "Sorry, we don't have this contact. Our list is growing, do check back soon"
+                print text
+                self.send(message.sender, text)
+
+#       This handles requests for prayer
         elif phrase_prayer:
-            text = "Thank you for reaching us. The Lord hears \n",
-            "his children"
+            text = "A prayer support partner will contact you shortly,"
+            +message.sender+"Remember: Blessed is he whose help is the God \n",
+            "of Jacob,whose hope is in the LORD his God, who made heaven and \n",
+            "earth, the sea, and all that is in them, who keeps faith forever;\n",
+            "- Psalm 146:5,6"
+            print text
+            self.send(message.sender, text)
+            
+#       This handles requests for Bible verses
         elif phrase_bible:
             my_text = rec_text.strip()
-            raw_book = my_text.split()[0]
+            raw_book, chapter_verse = my_text.split()
             book = raw_book.lower()
-            chapter_verse = my_text.split()[1]
-            chapter = chapter_verse.split(':')[0]
-            verse = chapter_verse.split(':')[1]
+            chapter, verse = chapter_verse.split(':')
+
             print book, chapter, verse
+            
             webpage = urllib2.urlopen('http://bible.cc/'+book+'/'+chapter+'-'+verse+'.htm').read()
-            stuffToSearch = ""
-            for line in webpage:
-                stuffToSearch += line
-            search_for = re.compile(r'<a href="http://kingjbible.com/'+book+'/'+chapter+'.htm">King James Bible</a></span><br>(.*)<p><span class="versiontext"><a href="http://kjv.us/'+book+'/'+chapter+'.htm">')
+            stuffToSearch = "".join(webpage)
+            search_for = r'<a href="http://kingjbible.com/'+book+'/'+chapter+'.htm">King James Bible</a></span><br>(.*)<p><span class="versiontext"><a href="http://kjv.us/'+book+'/'+chapter+'.htm">'
             search_it = re.search(search_for, stuffToSearch)
-            text = (search_it.group(1))
+            rawest_text = (search_it.group(1))
+            
+#           Sometimes King James italises words (insertions of translators)
+#           This removes the <i> and </i> html tags in such texts
+            raw_text = re.sub("<i>", "", rawest_text)
+            text = re.sub("</i>", "", raw_text)
             print text
+            self.send(message.sender, text)
+       
+#       This handles requests for Bible explanations
+        elif phrase_bible_explain:
+           book = rec_text.split()[1]
+           chapter_verse = rec_text.split()[2]
+           chapter, verse = chapter_verse.split(':')
+
+#          The following comments may be implemented later
+#          webpage = urllib2.urlopen('http://bible.cc/'+book+'/'+chapter+'-'+verse+'.htm').read()
+#          stuffToSearch = "".join(webpage)
+#          search_for = r'<a href="http://kingjbible.com/'+book+'/'+chapter+'.htm">King James Bible</a></span><br>(.*)<p><span class="versiontext"><a href="http://kjv.us/'+book+'/'+chapter+'.htm">'
+#          search_it = re.search(search_for, stuffToSearch)
+           my_set = book, chapter, verse
+           text = "One of our staff will soon reach you with an answer to your question on " + book + " " + chapter + ":" + verse + " . Thanks for using Vineyard!"
+           print text
+           self.send(message.sender, text) 
+#       This handles song requests (hymns for now)
+        elif phrase_hymn:
+            hymn_id = rec_text.split()[1]
+            text = "Thanks for the request. Hymn " + hymn_id + " has been added to the queue."       
+            print text
+            self.send(message.sender, text)
             
         else:
-            text = "Thanks for getting in touch"
+#           (If the message does not qualify for any of them)
+            text = "Sorry, your message is not understood. Please try again. Check for spelling and other errors."
+            print text
         
-        self.send(message.sender, text)
+            self.send(message.sender, text)
         
         
-
             
     def handle_call(self, message, caller, dt):
         print 'We received a call from %s at %s' % (caller, dt)
         number = caller
         text = "Thanks for the call, pastor john on %s" % \
         (caller)
-        
-        phrase = re.findall(r'pastor', text)
-        if phrase:
-            print 'yay'
-        else:
-            print 'nay'
         self.send(number, text)
 
 from simplesms import Modem
